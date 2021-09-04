88a3f7f4ee551c779c95d85512b13b6ee79f4fb5
MeetTandel <meettandel47@gmail.com>
2021-09-04 16:32:36 +0530

    all files

---
+++ README.md
@@ -0,0 +1,26 @@
+# mark 14 - Profit And Loss
+
+Profit and Loss will give you stock profit or loss based on the Current Price. Moreover, you can also know your Tesla stock at the current time.
+
+#### Live Project [https://lsu39.csb.app/](https://lsu39.csb.app/)
+
+## How to Use
+
+* Enter price of stock
+
+* Enter the quantity of stock
+
+* Enter Enter current price of stock
+
+* (Bonus) To get TESLA current price, click of get Tesla price button
+
+* Output will be shown after button is clicked
+
+
+## Tech Stack
+
+* HTML
+
+* CSS
+
+* Javascript
---
+++ index.js
@@ -0,0 +1,93 @@
+const input = document.querySelectorAll("input");
+const submitButton = document.querySelector("#submit-btn");
+const priceButton = document.querySelector(".current-price");
+const priceContainer = document.querySelector(".show-current-price");
+const outputContainer = document.querySelector(".output");
+const gifContainer = document.querySelector("#gif-container");
+const spanColor = document.querySelector(".output");
+const SERVER_URL =
+  "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=FGSONR9A73AEHCUF";
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let price;
+let date;
+
+priceButton.addEventListener("click", async () => {
+  input[2].style.display = "none";
+  try {
+    let response = await fetch(SERVER_URL);
+    let data = await response.json();
+    let firstKey = Object.keys(data)[1];
+    let firstDate = Object.keys(data[firstKey])[0];
+    date = firstDate.split("-");
+    price = Number(data[firstKey][firstDate]["1. open"]);
+    priceContainer.innerHTML = `<p>Current Price as of ${months[date[1] - 1]} ${
+      date[2]
+    } is <span>${price}</span45a52f></p>`;
+    return price;
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+submitButton.addEventListener("click", () => {
+  console.log(price);
+
+  const initialPrice = Number(input[0].value);
+  const quantity = Number(input[1].value);
+  const currentPrice = Number(input[2].value) | price;
+
+  calculateProfitAndLoss(initialPrice, quantity, currentPrice);
+});
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    let loss = (initial - current) * quantity;
+    let lossPercentage = (loss / initial) * 100;
+    showOutput("loss", loss, lossPercentage, "rgba(161, 37, 37, 0.5)");
+
+    if (lossPercentage > 50) {
+      showOutput("loss", loss, lossPercentage, "red");
+    }
+    gifContainer.innerHTML = showGif("https://giphy.com/embed/2rtQMJvhzOnRe");
+  } else if (current > initial) {
+    let profit = (current - initial) * quantity;
+    let profitPercentage = (profit / initial) * 100;
+
+    showOutput("profit", profit, profitPercentage, "green");
+    gifContainer.innerHTML = showGif(
+      "https://giphy.com/embed/26tPplGWjN0xLybiU"
+    );
+  } else {
+    showOutput(`<p>Neither safe nor secure</p>`);
+  }
+}
+
+function showGif(URL) {
+  return `<iframe
+  src=${URL}
+  width="300"
+  frameborder="0"
+  class="giphy-embed"
+  allowfullscreen
+  ></iframe>`;
+}
+function showOutput(message, value, percentValue, color) {
+  outputContainer.innerHTML = `<p>Your ${message} is <span>${value.toFixed(
+    2
+  )}</span> and the percent is <span>${percentValue.toFixed(2)}%</span></p>`;
+  spanColor.style.color = color;
+}
